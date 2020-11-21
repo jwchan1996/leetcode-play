a1366196b06a3cc27eab75f4ce49f9ab0d1a768e
jwchan1996 <741755613@qq.com>
2020-11-21 23:45:19 +0800

    104 二叉树的最大深度

--- 树/104 二叉树的最大深度/index.js
+++ 树/104 二叉树的最大深度/index.js
@@ -10,11 +10,13 @@
  * @return {number}
  */
 
-const maxDepth = root => {
-  // 判断边界问题
+var maxDepth = function(root) {
+  // 判断节点
   if (!root) {
+      // 当前节点为空则返回深度 0
       return 0
-  } 
-  // 递归取更深的子树深度加 1
-  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+  }
+  // 当前节点不为空则返回 1
+  // 并递归左右子树，取右子树高度的最大值进行累加
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
